9e00668cd7cef41e315fb95217520f22643210e0
michael_austin <austin.m@choiceenergymgt.com>
2022-09-01 10:55:01 -0500

    now displaying all video thumbnails

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -9,9 +9,9 @@ const Feed = () => {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
-      setVideos(data.items)
-    );
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => {
+      setVideos(data.items);
+    });
   }, [selectedCategory]);
 
   return (
