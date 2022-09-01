ca038c0b44a1f56ba09b556c7215955ecb902193
michael_austin <austin.m@choiceenergymgt.com>
2022-09-01 09:51:18 -0500

    created fetchFromAPI options & calls

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => (
   <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -12,13 +12,21 @@ const Feed = () => (
       }}
     >
       <Sidebar />
+
       <Typography
         className="copyright"
         variant="body2"
         sx={{ mt: 1.5, color: "#fff" }}
       >
-        Copyright 2022 JSM Media
+        Copyright 2022 MPA
       </Typography>
+    </Box>
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        New <span style={{ color: "#F31503" }}>videos</span>
+      </Typography>
+
+      <Videos videos={[]} />
     </Box>
   </Stack>
 );
